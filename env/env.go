b32690702f006fd1c152a732fc05b744e6ed29cc
John M <jwmwalrus@gmail.com>
2021-12-11 21:52:41 -0500

    Remove env.ParseArgs
    The function is too inflexible, so it's better to have it as a template.

--- env/env.go
+++ env/env.go
@@ -1,30 +1,8 @@
 package env
 
 import (
-	"io"
 	"os"
-
-	"github.com/pborman/getopt/v2"
-	log "github.com/sirupsen/logrus"
-	"gopkg.in/natefinch/lumberjack.v2"
 )
-
-// ParseArgs parses the given command line arguments
-func ParseArgs(logFile *lumberjack.Logger, echoLogging, verbose *bool, severity *string) (args []string) {
-	getopt.Parse()
-	args = getopt.Args()
-	arg0 := []string{os.Args[0]}
-	args = append(arg0, args...)
-
-	resolveSeverity(severity, verbose)
-
-	if *echoLogging {
-		mw := io.MultiWriter(os.Stderr, logFile)
-		log.SetOutput(mw)
-	}
-
-	return
-}
 
 // SetDirs Ensure that environment directories exist
 func SetDirs(cacheDir, configDir, dataDir, runtimeDir string) (err error) {
@@ -57,6 +35,31 @@ func SetDirs(cacheDir, configDir, dataDir, runtimeDir string) (err error) {
 	return
 }
 
+/*TEMPLATE
+
+import (
+	"github.com/pborman/getopt/v2"
+	log "github.com/sirupsen/logrus"
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+// ParseArgs parses the given command line arguments
+func ParseArgs(logFile *lumberjack.Logger, echoLogging, verbose *bool, severity *string) (args []string) {
+	getopt.Parse()
+	args = getopt.Args()
+	arg0 := []string{os.Args[0]}
+	args = append(arg0, args...)
+
+	resolveSeverity(severity, verbose)
+
+	if *echoLogging {
+		mw := io.MultiWriter(os.Stderr, logFile)
+		log.SetOutput(mw)
+	}
+
+	return
+}
+
 func resolveSeverity(severity *string, verbose *bool) {
 	givenSeverity := *severity
 
@@ -80,3 +83,4 @@ func resolveSeverity(severity *string, verbose *bool) {
 
 	return
 }
+*/
