ee6c689ccf6c0c7fe01ff04ea232772040b41f97
John M <jwmwalrus@gmail.com>
2023-03-31 23:03:38 -0500

    Add the CreateTheseDirs function

--- env/env.go
+++ env/env.go
@@ -1,86 +1,33 @@
 package env
 
 import (
+	"fmt"
 	"os"
 )
 
 // SetDirs Ensure that environment directories exist
 func SetDirs(cacheDir, configDir, dataDir, runtimeDir string) (err error) {
+	return CreateTheseDirs([]string{
+		cacheDir,
+		configDir,
+		dataDir,
+		runtimeDir,
+	})
+}
 
-	if _, err = os.Stat(cacheDir); os.IsNotExist(err) {
-		err = os.MkdirAll(cacheDir, 0755)
-		if err != nil {
-			return
+// CreateTheseDirs creates the non-existent directories from the given list
+//
+// This function will return at the first failure to create any of the
+// given directories.
+func CreateTheseDirs(dirs []string) (err error) {
+	for i, d := range dirs {
+		if _, err = os.Stat(d); os.IsNotExist(err) {
+			if err = os.MkdirAll(d, 0755); err != nil {
+				err = fmt.Errorf("failed to create dirs[%d] = %s: %w", i, d, err)
+				return
+			}
 		}
-	}
-
-	if _, err = os.Stat(configDir); os.IsNotExist(err) {
-		err = os.MkdirAll(configDir, 0755)
-		if err != nil {
-			return
-		}
-	}
-
-	if _, err = os.Stat(dataDir); os.IsNotExist(err) {
-		err = os.MkdirAll(dataDir, 0755)
-		if err != nil {
-			return
-		}
-	}
-
-	if _, err = os.Stat(runtimeDir); os.IsNotExist(err) {
-		err = os.MkdirAll(runtimeDir, 0755)
 	}
 
 	return
 }
-
-/*TEMPLATE
-
-import (
-	"github.com/pborman/getopt/v2"
-	log "github.com/sirupsen/logrus"
-	"gopkg.in/natefinch/lumberjack.v2"
-)
-
-// ParseArgs parses the given command line arguments
-func ParseArgs(logFile *lumberjack.Logger, echoLogging, verbose *bool, severity *string) (args []string) {
-	getopt.Parse()
-	args = getopt.Args()
-	arg0 := []string{os.Args[0]}
-	args = append(arg0, args...)
-
-	resolveSeverity(severity, verbose)
-
-	if *echoLogging {
-		mw := io.MultiWriter(os.Stderr, logFile)
-		log.SetOutput(mw)
-	}
-
-	return
-}
-
-func resolveSeverity(severity *string, verbose *bool) {
-	givenSeverity := *severity
-
-	if givenSeverity == "" {
-		if *verbose {
-			*severity = "info"
-		} else {
-			*severity = "error"
-		}
-	} else {
-		if _, err := log.ParseLevel(givenSeverity); err != nil {
-			*severity = "error"
-		} else {
-			*severity = givenSeverity
-		}
-	}
-
-	level, _ := log.ParseLevel(*severity)
-	log.SetLevel(level)
-	log.SetReportCaller(*severity == "debug")
-
-	return
-}
-*/
