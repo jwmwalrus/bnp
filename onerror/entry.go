a1dfb36088d0ea44afec2ac517431ee4e7d6ef3f
John M <jwmwalrus@gmail.com>
2023-08-26 18:22:35 -0500

    Rework onerror implementation
    Also:
    
    * Fixed onerror.Wit() mismatch in relation to slog.With()
    * Fixed missing Fatal() signature in onerror.Recorder interface

--- onerror/entry.go
+++ onerror/entry.go
@@ -1,54 +1,58 @@
 package onerror
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 	"os"
 	"runtime"
-	"strconv"
+	"time"
 
 	"github.com/jwmwalrus/bnp/httpstatus"
 )
 
+// Recorder defines the interface to record an error
 type Recorder interface {
+	Fatal(error)
 	LogHTTP(err error, r *http.Response, doNotCloseBody bool) error
 	Log(error)
 	Panic(error)
 	Warn(error)
-	With(...slog.Attr) Recorder
+	With(...any) Recorder
 }
 
+// NewRecorder returns a new recorder for the given slog.Logger
 func NewRecorder(l *slog.Logger) Recorder {
-	return &entry{l}
+	return &entry{Logger: l}
 }
 
 type entry struct {
 	*slog.Logger
+	caller []slog.Attr
+	pc     uintptr
 }
 
 // Fatal asserts that no error was given
 func (e *entry) Fatal(err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		e.withCaller(file, line).(*entry).
-			Logger.Error(err.Error())
+		if len(e.caller) == 0 {
+			e.caller, e.pc = callerAttrs()
+		}
+		e.logError(slog.LevelError, err)
 		os.Exit(1)
 	}
 }
 
 // LogHTTP logs and HTTP-related error
 func (e *entry) LogHTTP(err error, r *http.Response, doNotCloseBody bool) error {
+	var addRecord bool
 	if err != nil {
-		if r != nil {
-			e.withStatus(r.StatusCode, r.Status).(*entry).
-				Logger.Error(err.Error())
-		} else {
-			e.Logger.Error(err.Error())
-		}
-		return err
+		addRecord = true
 	} else if r != nil && httpstatus.IsError(r) {
+		addRecord = true
+
 		if !doNotCloseBody {
 			defer r.Body.Close()
 		}
@@ -57,28 +61,44 @@ func (e *entry) LogHTTP(err error, r *http.Response, doNotCloseBody bool) error 
 		b, _ = io.ReadAll(r.Body)
 		msg := string(b)
 
-		e.withStatus(r.StatusCode, r.Status, msg).(*entry).
-			Logger.Error(err.Error())
-		return fmt.Errorf("ERROR: %v\n\t%v", r.Status, msg)
+		err = fmt.Errorf(msg)
 	}
-	return nil
+
+	if !addRecord {
+		return nil
+	}
+
+	if len(e.caller) == 0 {
+		e.caller, e.pc = callerAttrs()
+	}
+
+	if r != nil {
+		a := statusAttrs(r.StatusCode, r.Status)
+		e.Logger = e.Logger.With(a...)
+	}
+
+	e.logError(slog.LevelError, err)
+
+	return err
 }
 
 // Log logs an error
 func (e *entry) Log(err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		e.withCaller(file, line).(*entry).
-			Logger.Error(err.Error())
+		if len(e.caller) == 0 {
+			e.caller, e.pc = callerAttrs()
+		}
+		e.logError(slog.LevelError, err)
 	}
 }
 
 // Panic asserts that no error was given
 func (e *entry) Panic(err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		e.withCaller(file, line).(*entry).
-			Logger.Error(err.Error())
+		if len(e.caller) == 0 {
+			e.caller, e.pc = callerAttrs()
+		}
+		e.logError(slog.LevelError, err)
 		panic(err)
 	}
 }
@@ -86,55 +106,38 @@ func (e *entry) Panic(err error) {
 // Warn warns on error
 func (e *entry) Warn(err error) {
 	if err != nil {
-		_, file, line, _ := runtime.Caller(1)
-		e.withCaller(file, line).(*entry).
-			Logger.Warn(err.Error())
+		if len(e.caller) == 0 {
+			e.caller, e.pc = callerAttrs()
+		}
+		e.logError(slog.LevelWarn, err)
 	}
 }
 
 // WithFields adds fields to the entry
-func (e *entry) With(a ...slog.Attr) Recorder {
-	return &entry{e.Logger.With(a)}
+func (e *entry) With(a ...any) Recorder {
+	if e.Logger != nil {
+		return &entry{Logger: e.Logger.With(a...)}
+	}
+	return &entry{Logger: slog.Default().With(a...)}
 }
 
-func (e *entry) withCaller(file string, line int) Recorder {
-	a := callerAttrs(file, line)
-	return e.With(a...)
+func (e *entry) logError(level slog.Level, err error) {
+	record := slog.NewRecord(time.Now(), level, "ONERROR-"+err.Error(), e.pc)
+	record.AddAttrs(e.caller...)
+	e.Logger.Handler().Handle(context.Background(), record)
 }
 
-func (e *entry) withStatus(statusCode int, status string, msg ...string) Recorder {
-	a := statusAttrs(statusCode, status, msg...)
-	return e.With(a...)
-}
-
-func callerAttrs(file string, line int) []slog.Attr {
+func callerAttrs() ([]slog.Attr, uintptr) {
+	pc, file, line, _ := runtime.Caller(2)
 	return []slog.Attr{
 		slog.String("caller", file),
 		slog.Int("callerLine", line),
-	}
+	}, pc
 }
 
-func statusAttrs(statusCode int, status string, msg ...string) []slog.Attr {
-	attr := []slog.Attr{
-		slog.Int("statusCode", statusCode),
-		slog.String("status", status),
+func statusAttrs(statusCode int, status string) []any {
+	return []any{
+		"status_code", statusCode,
+		"status", status,
 	}
-	if len(msg) == 1 {
-		attr = append(attr, slog.String("error", msg[0]))
-	} else if len(msg) > 0 {
-		for i, m := range msg {
-			attr = append(attr, slog.String("msg"+strconv.Itoa(i+1), m))
-		}
-	}
-	return attr
-}
-
-func withCaller(file string, line int) *entry {
-	a := callerAttrs(file, line)
-	return &entry{slog.With(a)}
-}
-
-func withStatus(statusCode int, status string, msg ...string) *entry {
-	a := statusAttrs(statusCode, status, msg...)
-	return &entry{slog.With(a)}
 }
