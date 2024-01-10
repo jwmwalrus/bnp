9860d36ddff9e05ba977ccf60494a1df8d222df7
John M <jwmwalrus@gmail.com>
2024-01-10 15:05:46 -0500

    Add more Pause-related functions

--- pause.go
+++ pause.go
@@ -6,9 +6,19 @@ import (
 	"os"
 )
 
-// Pause pauses the CLI
+// Pause pauses the CLI, issuing a message.
 func Pause(msg string) {
-	fmt.Printf("%s\n", msg)
+	Pausef("%s\n", msg)
+}
+
+// Pausef pauses the CLI, issuing a formatted string.
+func Pausef(format string, a ...any) {
+	fmt.Printf(format, a...)
 	fmt.Print("Press 'Enter' to continue...")
 	bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+// Pauseln pauses the CLI, issuing a list of arguments.
+func Pauseln(a ...any) {
+	Pausef("%s\n", fmt.Sprintln(a...))
 }
